e11095463fb19e39c0000c57f3479df7a99d8392
Tobias-G99 <65419332+Tobias-G99@users.noreply.github.com>
2020-08-09 15:02:38 +0200

    Ergebnisse und fertig gestelltes Programm
    Fertigstellung der Kommentierung im Programm und verwendete Ergebnisse. Ein Teil der Ergebnisse ist nicht final

---
+++ SU2_Gitter/Ergebnisse/L_av_vs_beta/chi_vs_beta.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as so
+
+x = np.loadtxt('chi_vs_beta.txt', usecols=0, skiprows=1)
+y = np.loadtxt('chi_vs_beta.txt', usecols=1, skiprows=1)
+dy = np.loadtxt('chi_vs_beta.txt', usecols=2, skiprows=1)
+
+plt.xticks(np.arange(min(x), max(x), 0.05))
+
+plt.xlabel(r'$\beta$')
+plt.ylabel(r'$\chi_{N_s}$')
+plt.ylim(min(y)-0.02,max(y)+3)
+plt.errorbar(x,y,yerr=dy,marker='+')
+plt.legend(loc='upper right')
+plt.show()
---
+++ SU2_Gitter/README.md
@@ -0,0 +1,14 @@
+Das beiliegende Programm entstand als Teil meiner Bachelorarbeit.
+Es ist nicht optimiert für lange Simulationen und großen Gittervolumina.
+
+
+Copyright:TBA
+
+Kompilierung:
+Linux:
+g++ -std=c++14 Monte_Carlo_SU2.cpp -o Monte_Carlo_SU2
+./Monte_Carlo_SU2
+
+
+Windows:
+TBA
